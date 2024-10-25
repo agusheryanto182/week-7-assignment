991f631e0142d29318965538a5bb8a0507f2ce41
Agus Heryanto <agusheryantodev@gmail.com>
2024-10-25 22:38:47 +0700

    feat(make file): adding a make file feature

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const fs = require("node:fs")
 const readline = require('node:readline');
+const helper = require("./helper")
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 const app = {}
 
-// contoh script pembuatan folder
- app.makeFolder = () => {
-    rl.question("Masukan Nama Folder : ",(folderName) => {
-        fs.mkdir(__dirname + `/${folderName}`,() => {
+app.makeFolder = () => {
+    rl.question("Masukan Nama Folder : ", (folderName) => {
+        fs.mkdir(__dirname + `/${folderName}`, () => {
             console.log("success created new folder");
-            
+
         })
         rl.close()
     })
-} 
+}
 
-// To Do : lanjutkan pembuatan logic disini 
+app.makeFile = () => {
+    rl.question("Masukan Nama File : ", (fileName) => {
+        fs.writeFile(__dirname + `/${fileName}`, "", () => {
+            console.log("success created new file");
+        })
+        rl.close()
+    })
+}
 
 
 module.exports = app
